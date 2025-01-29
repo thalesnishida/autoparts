a24c317e82a2879cca366820c2a528908af4a555
Thales Nishida <medeiros.tn@gmail.com>
2025-01-29 08:26:38 -0300

    first commit

---
+++ src/test/kotlin/br/com/thalesnishida/autoparts/AutopartsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.thalesnishida.autoparts
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutopartsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
